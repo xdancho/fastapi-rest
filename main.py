797f149125c4a2fe3f82b744e0bb1131dce17560
Yordan Kostadinov <yordan.k@siteground.com>
2023-03-02 13:52:35 +0200

    logging

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import sys
-
 from fastapi import FastAPI
 
-version = f"{sys.version_info.major}.{sys.version_info.minor}"
+# Configure logs
+import logging
+from fastapi.logger import logger as fastapi_logger
+gunicorn_error_logger = logging.getLogger("gunicorn.error")
+gunicorn_logger = logging.getLogger("gunicorn")
+uvicorn_access_logger = logging.getLogger("uvicorn.access")
+uvicorn_access_logger.handlers = gunicorn_error_logger.handlers
+
+fastapi_logger.handlers = gunicorn_error_logger.handlers
+
+if __name__ != "__main__":
+    fastapi_logger.setLevel(gunicorn_logger.level)
+else:
+    fastapi_logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
 @app.get("/")
-async def home():
-    message = f"Hello world! From FastAPI running on Uvicorn with Gunicorn. Using Python {version}"
-    return {"message": message}
+async def func_home():
+    logger = logging.getLogger("gunicorn.error")
+    logger.info("info")
+    logger.warning("warning")
+    logger.error("error")
+    logger.error("loglevel="+logging.getLevelName(logger.getEffectiveLevel()))
+    return {"message": "index"}
 
 @app.get("/ping")
-async def home():
+async def func_ping():
     return {"message": "pong"}
